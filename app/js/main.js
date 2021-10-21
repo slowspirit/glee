2c34d426a89de54cf910b0b657723af051df3351
slowspirit <slowspirit@gmail.com>
2021-10-21 12:10:14 +0300

    added shop page 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,28 @@
 $(function () {
+
+  $(".star").rateYo({
+    rating: 3.6,
+    starWidth: "12px",
+    readOnly: true,
+    spacing: "7px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="12px" height="11px" viewBox="0 0 12 11" version="1.1"><path d="M 5.402344 0.382812 L 4.042969 3.226562 L 0.996094 3.683594 C 0.453125 3.765625 0.234375 4.460938 0.628906 4.859375 L 2.832031 7.070312 L 2.308594 10.195312 C 2.214844 10.761719 2.792969 11.183594 3.277344 10.921875 L 6 9.445312 L 8.722656 10.921875 C 9.207031 11.183594 9.785156 10.761719 9.691406 10.195312 L 9.167969 7.070312 L 11.371094 4.859375 C 11.765625 4.460938 11.546875 3.765625 11.003906 3.683594 L 7.957031 3.226562 L 6.597656 0.382812 C 6.355469 -0.125 5.648438 -0.132812 5.402344 0.382812 Z M 5.402344 0.382812 " /></svg>'
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    step: 0.01,
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -12,12 +36,12 @@ $(function () {
   var containerEl2 = document.querySelector('[data-ref="design__mix"]');
 
   var config = {
-      controls: {
-          scope: 'local'
-      }
+    controls: {
+      scope: 'local'
+    }
   };
 
   var mixer1 = mixitup(containerEl1, config);
   var mixer1 = mixitup(containerEl2, config);
-  
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss          = require('gulp-sass')(require('sass'));
-const concat        = require('gulp-concat');
-const autoprefixer  = require('gulp-autoprefixer');
-const uglify        = require('gulp-uglify');
-const imagemin      = require('gulp-imagemin');
-const browserSync   = require('browser-sync').create();
-const del           = require('del');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const del = require('del');
 
 
 function browsersync() {
@@ -19,16 +19,16 @@ function browsersync() {
 }
 
 
-function styles(){
+function styles() {
   return src('app/scss/style.scss')
-  .pipe(scss({outputStyle: 'compressed'}))
-  .pipe(concat('style.min.css'))
-  .pipe(autoprefixer({
-    overrideBrowserslist: ['last 10 versions'],
-    grid: true
-  }))
-  .pipe(dest('app/css'))
-  .pipe(browserSync.stream())
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function scripts() {
@@ -37,28 +37,30 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
 function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-	  imagemin.mozjpeg({quality: 75, progressive: true}),
-	  imagemin.optipng({optimizationLevel: 5}),
-	  imagemin.svgo({
-      plugins: [
-        {removeViewBox: true},
-        {cleanupIDs: false}
-      ]
-    })
-  ]))
-  .pipe(dest('dist/images'))
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(dest('dist/images'))
 }
 
 
@@ -73,8 +75,8 @@ function build() {
     'app/**/*.html',
     'app/css/style.min.css',
     'app/js/main.min.js',
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+  ], { base: 'app' })
+    .pipe(dest('dist'))
 }
 
 
